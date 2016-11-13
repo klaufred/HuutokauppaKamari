4582a595604f30707b565ae61ecc803a8da3d48b
klaufred <klaus.fredriksson@helsinki.fi>
2016-11-13 14:32:56 +0200

    testi

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,10 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
   }
